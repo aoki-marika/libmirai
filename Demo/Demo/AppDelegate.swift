687fd1907c8bebbd59a077c50dbc1d59cef26cde
marika <marika@waifu.club>
2020-01-18 07:36:46 -0400

    Add empty demo macOS project.

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Marika on 2020-01-18.
+//  Copyright © 2020 Marika. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject {
+
+}
+
+// MARK: - NSApplicationDelegate
+
+extension AppDelegate: NSApplicationDelegate {
+
+}
