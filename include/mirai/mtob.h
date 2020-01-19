73215d590dbf147120abdf069c3fd8175bee4888
marika <marika@waifu.club>
2020-01-19 07:16:24 -0400

    Add MTOB skeleton.

---
+++ include/mirai/mtob.h
@@ -0,0 +1,31 @@
+//
+//  mtob.h
+//  libmirai
+//
+//  Created by Marika on 2020-01-19.
+//  Copyright © 2020 Marika. All rights reserved.
+//
+
+#pragma once
+
+#include <stdio.h>
+
+// MARK: - Data Structures
+
+/// The data structure for an MTOB file that has been opened.
+struct mtob_t
+{
+};
+
+// MARK: - Functions
+
+/// Open the MTOB file at the current offset of the given file handle into the given MTOB.
+/// @param file The file handle to open the MTOB file from.
+/// @param mtob The MTOB to open the file into.
+void mtob_open(FILE *file, struct mtob_t *mtob);
+
+/// Close the given MTOB, releasing all of it's allocated memory.
+///
+/// This must be called after an MTOB is opened and before program execution completes.
+/// @param mtob The MTOB to close.
+void mtob_close(struct mtob_t *mtob);
---
+++ src/mtob.c
@@ -0,0 +1,19 @@
+//
+//  mtob.c
+//  libmirai
+//
+//  Created by Marika on 2020-01-19.
+//  Copyright © 2020 Marika. All rights reserved.
+//
+
+#include "mtob.h"
+
+// MARK: - Functions
+
+void mtob_open(FILE *file, struct mtob_t *material)
+{
+}
+
+void mtob_close(struct mtob_t *material)
+{
+}
