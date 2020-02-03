37be5e2b85eb96c01ed1e5e01d5d3b02ad7fd436
marika <marika@waifu.club>
2020-02-03 11:45:39 -0400

    Ensure GLFW uses at least OpenGL 3.2 Core.

--- viewer/src/main.c
+++ viewer/src/main.c
@@ -83,10 +83,12 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
 
     // create the window
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Viewer", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(400, 240, path, NULL, NULL);
     if (!window)
     {
         glfwTerminate();
