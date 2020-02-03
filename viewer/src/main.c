e3a65581033d4742e26dad95ded43068c1dd6f2e
marika <marika@waifu.club>
2020-02-03 08:47:02 -0400

    Add empty viewer project with GLFW.

---
+++ viewer/src/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  viewer
+//
+//  Created by Marika on 2020-02-03.
+//  Copyright © 2020 Marika. All rights reserved.
+//
+
+#include <stdlib.h>
+
+// MARK: - Functions
+
+int main(int argv, char **argc)
+{
+    return EXIT_SUCCESS;
+}
