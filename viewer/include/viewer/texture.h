e2577b4cb1896bb8c3567943e023b821230712e7
marika <marika@waifu.club>
2020-02-23 10:25:02 -0400

    Add some clarification to texture_upload(struct ctr_texture_t *, FILE *, GLuint *) documentation.

--- viewer/include/viewer/texture.h
+++ viewer/include/viewer/texture.h
@@ -15,7 +15,9 @@
 // MARK: - Functions
 
 /// Decode and upload the given texture's data to an OpenGL texture.
-/// @param texture The texture to decode and upload the data of.
+///
+/// During this function the texture is bound to `GL_TEXTURE_2D` on the active texture unit.
+/// @param texture The CTR texture to decode and upload the data of.
 /// @param file The file handle containing the data of the given texture.
 /// @param id The value to set to the created OpenGL texture's ID.
 void texture_upload(struct ctr_texture_t *texture, FILE *file, GLuint *id);
