046be9bd291a6c066ec74152688b2b8366ee3216
marika <marika@waifu.club>
2020-02-23 10:53:03 -0400

    Add texture uploading to spr_viewer.

--- viewer/src/spr_viewer.c
+++ viewer/src/spr_viewer.c
@@ -8,17 +8,35 @@
 
 #include "spr_viewer.h"
 
-// MARK: - Functions
+#include "texture.h"
+
+// MARK: - Textures
 
 void spr_viewer_run(GLFWwindow *window,
                     struct spr_t *spr,
                     const struct program_t *program2d)
 {
+    // upload each texture to a separate unit
+    // use a base unit to allow the first n units to be used for debug ui
+    const int spr_base_unit = GL_TEXTURE5;
+    GLuint texture_ids[spr->num_textures];
+    for (int i = 0; i < spr->num_textures; i++)
+    {
+        glActiveTexture(spr_base_unit + i);
+        texture_upload(&spr->textures[i],
+                       spr->file,
+                       &texture_ids[i]);
+    }
+
     // run the main loop
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // delete all the textures
+    glDeleteTextures(spr->num_textures, texture_ids);
 }
