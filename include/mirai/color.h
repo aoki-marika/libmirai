5df36c82e77d4255856e8228e27c86cab66fc11d
marika <marika@waifu.club>
2020-03-10 08:21:26 -0300

    Remove the now unused color4_t functions.

--- include/mirai/color.h
+++ include/mirai/color.h
@@ -17,15 +17,3 @@ struct color4_t
 {
     float r, g, b, a;
 };
-
-// MARK: - Functions
-
-/// Read the floating point RGBA colour at the current offset of the given file handle into the given colour.
-/// @param file The file handle to read the floating point RGBA colour from.
-/// @param color The colour to read the file into.
-void color4_readf(FILE *file, struct color4_t *color);
-
-/// Read the unsigned 8-bit RGBA colour at the current offset of the given file handle into the given colour.
-/// @param file The file handle to read the unsigned 8-bit RGBA colour from.
-/// @param color The colour to read the file into.
-void color4_readu8(FILE *file, struct color4_t *color);
